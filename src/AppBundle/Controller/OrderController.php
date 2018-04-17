b7105587e7b415df42c58cca9d2e441266961a7a
Atala <alois.guillope@gmail.com>
2018-04-17 16:23:12 +0200

    fix: enable payment to be retried on error

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -12,6 +12,7 @@ use AppBundle\Form\StripePaymentType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sylius\Component\Payment\Model\PaymentInterface;
+use Sylius\Component\Payment\PaymentTransitions;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -26,11 +27,13 @@ class OrderController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $cart = $this->get('sylius.context.cart')->getCart();
+        $order = $this->get('sylius.context.cart')->getCart();
+
+        $orderManager = $this->get('coopcycle.order_manager');
 
         // TODO Check if cart is empty
 
-        $deliveryAddress = $cart->getShippingAddress();
+        $deliveryAddress = $order->getShippingAddress();
 
         $form = $this->createForm(DeliveryAddressType::class, $deliveryAddress);
 
@@ -41,29 +44,41 @@ class OrderController extends Controller
             $this->getDoctrine()->getManagerForClass(Address::class)->persist($deliveryAddress);
             $this->getDoctrine()->getManagerForClass(Address::class)->flush();
 
-            return $this->redirectToRoute('order_payment');
+            // TODO Set customer via listeners
+            $order->setCustomer($this->getUser());
+
+            // Create order, to generate a number
+            $orderManager->create($order);
+
+            $this->get('sylius.manager.order')->flush();
+
+            return $this->redirectToRoute('order_payment', array('number' => $order->getNumber()));
         }
 
         return array(
-            'order' => $cart,
+            'order' => $order,
             'form' => $form->createView(),
-            'restaurant' => $cart->getRestaurant(),
+            'restaurant' => $order->getRestaurant(),
             'deliveryAddress' => $deliveryAddress,
         );
     }
 
     /**
-     * @Route("/payment", name="order_payment")
+     * @Route("/payment/{number}", name="order_payment")
      * @Template()
      */
-    public function paymentAction(Request $request)
+    public function paymentAction($number, Request $request)
     {
-        $orderManager = $this->get('coopcycle.order_manager');
-        $settingsManager = $this->get('coopcycle.settings_manager');
 
-        $order = $this->get('sylius.context.cart')->getCart();
+
+        $order = $this->get('sylius.repository.order')->findOneBy([
+            'number' => $number
+        ]);
 
         $stripePayment = $order->getLastPayment(PaymentInterface::STATE_CART);
+
+        $stateMachineFactory = $this->get('sm.factory');
+        $stateMachine = $stateMachineFactory->get($stripePayment, PaymentTransitions::GRAPH);
 
         $form = $this->createForm(StripePaymentType::class);
 
@@ -79,11 +94,7 @@ class OrderController extends Controller
 
             $stripePayment->setStripeToken($form->get('stripeToken')->getData());
 
-            // TODO Set customer via listeners
-            $order->setCustomer($this->getUser());
-
-            // Create order, to generate a number
-            $orderManager->create($order);
+            $stateMachine->apply(PaymentTransitions::TRANSITION_CREATE);
 
             $this->get('sylius.manager.order')->flush();
 
